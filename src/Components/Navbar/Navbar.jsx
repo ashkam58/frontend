e38b6acfc4f1141f3aa68af5d4cb9a7a19889365
ashkam58 <ashkam58@gmail.com>
2024-01-18 23:46:36 +0530

    error removed by importing LINK for the react router DOM

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
